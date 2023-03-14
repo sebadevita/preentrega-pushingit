1b1d465c65e2b10e524dd6286df93f4cd4b5f62c
Sebastian De Vita <sebassdevita@gmail.com>
2023-03-14 20:12:32 -0300

    Issue-2 resolved

--- cypress/support/pages/shoppingCartPage.js
+++ cypress/support/pages/shoppingCartPage.js
@@ -3,6 +3,10 @@ export class ShoppingCartPage {
         cy.xpath('//button').contains('Show total price').click()
 
     }
+    
+    returnProductName(product){
+        return cy.get('p').contains(product)
+    }
 
     returnProductPrice(product){
         return cy.get('p').contains(product).siblings('p#productPrice')
